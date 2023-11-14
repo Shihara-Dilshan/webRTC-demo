35dd424d593bfc55187aab305fd3d225447ddedc
shihara <shihara@surge.global>
2023-11-15 02:32:53 +0530

    chore: minor changes to call view

--- App.tsx
+++ App.tsx
@@ -1,10 +1,9 @@
 import React, {useState} from 'react';
-import {Button, SafeAreaView, View, StatusBar} from 'react-native';
+import {Button, SafeAreaView, View} from 'react-native';
 import {mediaDevices, MediaStream, RTCView} from 'react-native-webrtc';
 
 const App = () => {
   const [localStream, setLocalStream] = useState<MediaStream | null>(null);
-  const [clientStream, setClientStream] = useState<MediaStream | null>(null);
 
   const start = async () => {
     if (!localStream) {
@@ -17,23 +16,27 @@ const App = () => {
       }
     }
   };
+
   const stop = () => {
     localStream?.release();
     setLocalStream(null);
   };
   return (
-    <>
-      <StatusBar barStyle="dark-content" />
-      <SafeAreaView style={{flex: 1}}>
-        {localStream && (
-          <RTCView streamURL={localStream.toURL()} style={{flex: 1}} />
-        )}
-        <View>
-          <Button title="Start" onPress={start} />
-          <Button title="Stop" onPress={stop} />
-        </View>
-      </SafeAreaView>
-    </>
+    <SafeAreaView style={{flex: 1}}>
+      {localStream && (
+        <RTCView streamURL={localStream.toURL()} style={{flex: 1}} />
+      )}
+      <View
+        style={{
+          position: 'absolute',
+          bottom: 0,
+          width: '100%',
+          paddingHorizontal: 5,
+        }}>
+        <Button title="Start" onPress={start} />
+        <Button title="Stop" onPress={stop} />
+      </View>
+    </SafeAreaView>
   );
 };
 
